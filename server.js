02ec36b1fbb79d402d492744db1771ffa7025217
YoussefFigary <144602797+YoussefFigary@users.noreply.github.com>
2025-12-15 23:30:46 +0200

    search  bar finished but needs the correct back ends

--- server.js
+++ server.js
@@ -10,10 +10,27 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.render('category', { name: 'Chris' });
+    res.render("searchresults", { query: "" });
 });
 app.get('/about', (req, res) => {
     res.render('registration', { name: 'Chris' });
+});
+
+app.get("/search", (req, res) => {
+  const query = req.query.q || "";
+  res.render("searchresults", { query });
+});
+
+app.get("/api/search", (req, res) => {
+  const query = req.query.query || "";
+  // هنا استخدم data حقيقية أو dummy array
+  const products = [
+    "rome",
+    "paris",
+    "santorini"
+  ];
+  const results = products.filter(p => p.toLowerCase().includes(query.toLowerCase()));
+  res.json(results);
 });
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
